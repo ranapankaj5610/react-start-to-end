b63cb7ee52c2b1cbdc005bb0b0a74637739ac40d
ranapankaj5610 <ranapankaj5610.com>
2023-12-25 00:07:18 +0530

    Password Generator

---
+++ passwordGenerator/README.md
@@ -0,0 +1,11 @@
+# Password Generator
+
+This is a basic Password Generator project in which we are randomly generating the password by using numbers, special characters and alphabates as per our requirement. Numbers and special characters are optional we can use them as per our choice.
+We are using the hooks like:- useState, useEffect, useCallback to make our project more optimized.
+We had added a copy button, which is used for copy the generated password to the clipboard.
+
+Clone the project:-
+
+1. Go to the project directory
+2. run "npm install"
+3. run "npm run dev"
---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,97 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null); //Here the use of the ref hook is just to give the user better experience while clicking 5he copy buttton the copied text get higlighted.
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    //Callback hook is basically used to memonize the function such that when we repeat the process it will take the saved values from cache....it is basically used to optimize the process.
+    passwordRef.current?.select(); //It higlight the copied text
+    passwordRef.current?.setSelectionRange(0, 999); //Here we can set the range of the copied text in length not more than 999 character get copied.
+    window.navigator.clipboard.writeText(password); //it will copy the character to clipboard.
+  }, [password]);
+
+  useEffect(() => {
+    //It is used to call the function first time while just running the app.
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+  return (
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+        >
+          copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev); //Here we are just using the toggling, just change the previous state.
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
