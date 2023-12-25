31149f94f6fe0598c30ee7f84482436ac6376654
ranapankaj5610 <ranapankaj5610.com>
2023-12-25 13:03:38 +0530

    Currency Convertor Project

---
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency])); //we can also use res.currency beacuse we know that the res is a object of currency as key
+  }, [currency]);
+  return data;
+}
+
+export default useCurrencyInfo;
